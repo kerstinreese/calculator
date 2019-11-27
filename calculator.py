e54c891095167811c132e7e49d7fdebd2b015b64
Lukas Wachter <lukas.wachter96@gmail.com>
2019-11-27 15:11:34 +0100

    hello

--- calculator.py
+++ calculator.py
@@ -1,2 +1,12 @@
-First
-Second
+class Calculator:
+    def add(x, y):
+        print(x + y)
+
+    def sub(x, y):
+        print(x - y)
+
+    def increment(x):
+        print(x + 1)
+
+    def help():
+        print("Hallo")
